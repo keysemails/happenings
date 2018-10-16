0e4329c75e6dffd1a248360909fa8725b99939bc
Tim Goodwin <timg.goodwin@gmail.com>
2018-10-15 21:07:50 -0400

    conjugating notification titles

--- app/components/inbox/Notification.jsx
+++ app/components/inbox/Notification.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
+import * as NotificationTypes from '../../constants/notificationTypes';
 
 class Notification extends React.Component {
 	formatDate = (datestr) => {
@@ -9,17 +11,50 @@ class Notification extends React.Component {
 		const withinOneWeek = moment().add(7, 'days') > parsed;
 		return withinOneWeek ? parsed.calendar() : parsed.format('dddd MMM Do');
 	}
+	conjugateNotification = (notification, event) => {
+		const userLink = (
+			<Link to={`/user/${notification.username}`}>{notification.username}</Link>
+		);
+		switch(notification.type) {
+			case (NotificationTypes.INVITE_TO_EVENT):
+				return (<span>{userLink} invited you to:</span>)
+			case (NotificationTypes.SHARE_EVENT):
+				const authorLink = (
+					<Link to={`/user/${event.author.username}`}>{event.author.username}</Link>
+				);
+				return (<span>{userLink} shared {authorLink}'s event:</span>
+				)
+			case (NotificationTypes.EVENT_TIME_CHANGE):
+				return (
+					<span>Time Change:</span>
+				)
+			case (NotificationTypes.EVENT_CANCELLED):
+				return (
+					<span>CANCELLED:</span>
+				)
+			case (NotificationTypes.USER_ATTENDING):
+				return (
+					<span>{userLink} is attending:</span>
+				)
+			case (NotificationTypes.USER_COMMENTED):
+				return (
+					<span>{userLink} commented:</span>
+				)
+		}
+	}
 	render() {
 		const { id, notification, event, markAsRead } = this.props;
 		const classname = classNames('notification', {'unread': !notification.read});
 		return (
-			<div className={classname} onClick={() => markAsRead(id)}>
+			<div className={classname} onClick={() => notification.read ? null : markAsRead(id)}>
 				<div className='notification-title'>
-					{notification.username} {notification.type}
+					{this.conjugateNotification(notification, event)}
 				</div>
 				<div className='notification-body'>
 					<div>
-						<img className='notification-img' src={event.thumb_url}></img>
+						<Link to={`/event/${notification.postId}`}>
+							<img className='notification-img' src={event.thumb_url}></img>
+						</Link>
 					</div>
 					<div className='event-details'>
 						{event.title}<br/>
