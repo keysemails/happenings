ea5a8deeccb63612af6fa0e0e69463afe965695c
Tim Goodwin <timg.goodwin@gmail.com>
2019-03-14 14:25:14 -0400

    refactor users view to use pgBouncer promises instead of callback funcs

--- api/views/users.js
+++ api/views/users.js
@@ -2,81 +2,72 @@ const getPool = require('../connection').getConnectionPool;
 const pool = getPool();
 
 
+const handle500 = (err, response) => {
+  console.error('FUCK');
+  console.error(err);
+  return response.status(500);
+}
+
+
 const createUser = (request, response) => {
   const { username, email, user_type, bio, is_private } = request.body;
   pool.query(
     'insert into users (username, email, user_type, bio, is_private) \
-    values ($1, $2, $3, $4, $5)',
-    [username, email, user_type, bio, is_private],
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      response.status(201).send('user created successfully');
-  });
+    values ($1, $2, $3, $4, $5) returning id',
+    [username, email, user_type, bio, is_private]
+  ).then(result => {
+    response.status(201).send(`user created successfully with id ${result.rows[0].id}`)
+  }).catch(err => handle500(err, response));
 }
 
 
 const updateUser = (request, response) => {
   const uid = parseInt(request.params.uid);
   const {username, email, user_type} = request.body;
-  pool.query('update users set username = $1, email = $2, user_type = $3 where id = $4',
-    [name, email, user_type, uid],
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      response.status(200).send(`user modified with id ${uid}`);
-  });
+  pool.query(
+    'update users set username = $1, email = $2, user_type = $3 where id = $4',
+    [name, email, user_type, uid]
+  ).then(results => {
+    response.status(200).send(`user modified with id ${uid}`)
+  }).catch(err => handle500(err, response));
 }
 
 
 const deleteUser = (request, response) => {
   const uid = parseInt(request.params.uid);
   pool.query('delete from users where id = $1',
-    [uid],
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      response.status(200).send(`user with id ${uid} has been deleted. see ya!`);
-  });
+    [uid]
+  ).then(results => {
+    response.status(200).send(`user with id ${uid} has been deleted. see ya!`);
+  }).catch(err => handle500(err, response));
 }
 
 
 const getUsers = (request, response) => {
-    pool.query('select * from users order by id asc',
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        response.status(200).json(results.rows);
-    });
+    pool.query('select * from users order by id asc')
+    .then(results => {
+      response.status(200).json(results.rows)
+    }).catch(err => handle500(err, response));
 }
 
 
 const getUserByUid = (request, response) => {
   const uid = request.params.uid;
   pool.query('select * from users where id = $1',
-    [id],
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      response.status(200).json(results.rows);
-    });
+    [id]
+  ).then(results => {
+    response.status(200).json(results.rows)
+  }).catch(err => handle500(err, response));
 }
+
 
 const getUserByUsername = (request, response) => {
     const username = request.params.username;
     pool.query('select * from users where username = $1',
-      [username],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
-        response.status(200).json(results.rows);
-    });
+      [username]
+    ).then(results => {
+      response.status(200).json(results.rows);
+    }).catch(err => handle500(err, response));
 }
 
 module.exports = {
