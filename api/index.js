26cdd6899f473faf4410249f61f822b7cd707034
Tim Goodwin <timg.goodwin@gmail.com>
2019-03-14 14:19:11 -0400

    add CRUD endpoints for post data

--- api/index.js
+++ api/index.js
@@ -16,13 +16,29 @@ app.get('/', (req, res) => {
     res.json({info: 'node.js, express, and postgres api'})
 });
 
-const users = require('./views/users');
-app.get('/users', users.getUsers);
-app.get('/users/:username', users.getUserByUsername);
-app.post('/users', users.createUser);
-app.put('/users/:uid', users.updateUser);
-app.delete('/users/:uid', users.deleteUser);
+const userViews = require('./views/users');
+const postViews = require('./views/posts');
+
+
+/**
+ * USER ENDPOINTS
+ */
+app.get('/users', userViews.getUsers);
+app.get('/users/:username', userViews.getUserByUsername);
+app.get('/users/:username/posts', postViews.getUserPosts);
+app.post('/users', userViews.createUser);
+app.put('/users/:uid', userViews.updateUser);
+app.delete('/users/:uid', userViews.deleteUser);
+
+/**
+ * POST ENDPOINTS
+ */
+app.post('/posts', postViews.createPost);
+app.get('/posts/:post_id', postViews.getPost);
+app.put('/posts/:post_id', postViews.updatePost);
+app.delete('/posts/:post_id', postViews.deletePost);
 
 app.listen(port, () => {
     console.log(`app running on port ${port}.`);
 });
+
